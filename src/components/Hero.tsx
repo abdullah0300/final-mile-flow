09d450dfea2ded67da4607aca0c74c19a724d1a4
abdullah0300 <aslamabdullah0300@gmail.com>
2025-09-16 03:35:43 +0500

    settleddd

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { Truck, Clock, MapPin } from "lucide-react";
 import heroImage from "@/assets/hero-logistics.jpg";
 const Hero = () => {
   return (
-    <section className="relative min-h-screen flex items-center mt-8 md:mt-18 justify-center bg-gradient-to-br from-logistics-blue to-logistics-blue-light overflow-hidden">
+    <section className="relative min-h-screen flex items-center mt-4 md:mt-18 justify-center bg-gradient-to-br from-logistics-blue to-logistics-blue-light overflow-hidden">
       {/* Background Image with Overlay */}
       <div className="absolute inset-0">
         <img
